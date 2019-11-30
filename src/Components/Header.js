50964d011cb649b60485c910b1184281bd345a44
muhammadluth <muhammadluthfi059@gmail.com>
2019-11-30 10:22:55 +0700

    first-commit

---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div id="header">
+        <div className="image-backgroud">
+          <div className="overlay-view"></div>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/Screens/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Header from "../Components/Header";
+import '../Assets/styles.css'
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <div className='container-fluid'>
+
+        </div>
+      </div>
+    );
+  }
+}
